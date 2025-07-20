67925ede57ea12778081361cfbbdbd158421672e
dwd9999 <ojw1010@gmail.com>
2025-07-20 22:21:56 +0900

    Initial Commit

---
+++ src/main/java/com/mysomething/MysomethingBeApplication.java
@@ -0,0 +1,13 @@
+package com.mysomething;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysomethingBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysomethingBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mysomething/MysomethingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysomething;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysomethingBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
